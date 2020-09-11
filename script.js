2a96cc8aceae59cf60714496196e58c8141facf0
Karan <karanoza94@gmail.com>
2020-09-11 12:55:45 +0530

    initialize project

---
+++ script.js
@@ -0,0 +1,19 @@
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+// initially get fav movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+  const resp = await fetch(url);
+  const respData = await resp.json();
+
+  console.log(respData);
+}
